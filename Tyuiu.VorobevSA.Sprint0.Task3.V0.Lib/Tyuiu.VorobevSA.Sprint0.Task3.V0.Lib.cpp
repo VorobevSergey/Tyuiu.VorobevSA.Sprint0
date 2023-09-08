175bdbe7bf88ac5e433f70b337a42314d230903e
Tyuiu.VorobevSA@outlook.com <vorobewserezha2118@gmail.com>
2023-09-08 22:48:18 +0500

    Task3

---
+++ Tyuiu.VorobevSA.Sprint0.Task3.V0.Lib/Tyuiu.VorobevSA.Sprint0.Task3.V0.Lib.cpp
@@ -0,0 +1,15 @@
+// Tyuiu.VorobevSA.Sprint0.Task3.V0.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: This is an example of a library function
+class Service :public ISprint0Task3
+{
+	virtual int SummV3(int a, int b, int c) override
+	{
+		return a + b + c;
+	};
+};
